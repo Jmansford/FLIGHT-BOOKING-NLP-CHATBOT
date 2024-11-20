38c38fbf69dbf3b8bbb8d2f6107f87a3738c3efa
Jai Mansford <jai.mansford@provenworks.com>
2024-11-20 17:52:28 +0000

    moved pre-processing into intent_matching

--- Classes/intent_matching.py
+++ Classes/intent_matching.py
@@ -1,8 +1,46 @@
-import json
+import json, re, nltk
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.metrics.pairwise import cosine_similarity
-from Classes.preprocessing import preprocess_input
 from nltk.corpus import wordnet
+from nltk import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+nltk.download('wordnet', quiet = True)
+nltk.download('averaged_perceptron_tagger', quiet = True)
+nltk.download('punkt', quiet = True)
+
+
+lemmatizer = WordNetLemmatizer()
+
+# Preprocess user input
+def preprocess_input(user_input):
+    # Convert to lowercase
+    user_input = user_input.lower()
+    # Remove punctuation
+    user_input = re.sub(r"['\.,!?]", "", user_input)
+    # Remove newline characters
+    user_input = user_input.replace("\n", " ")
+    # Tokenize input
+    tokens = word_tokenize(user_input)
+    # POS tagging
+    pos_tags = nltk.pos_tag(tokens)
+
+    # Function to convert POS tag to a format that WordNetLemmatizer can understand
+    def get_wordnet_pos(tag):
+        if tag.startswith('J'):
+            return wordnet.ADJ
+        elif tag.startswith('V'):
+            return wordnet.VERB
+        elif tag.startswith('N'):
+            return wordnet.NOUN
+        elif tag.startswith('R'):
+            return wordnet.ADV
+        else:
+            return wordnet.NOUN
+
+    # Lemmatize tokens with POS tags
+    tokens = [lemmatizer.lemmatize(token, get_wordnet_pos(tag)) for token, tag in pos_tags]
+    return tokens
 
 # Load intents from JSON file
 def load_intents(json_file='Resources/intents.json'):
